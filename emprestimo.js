9f37a952616b04fd5833435c847ae47174bcbbd0
LUISA-PC\LUISA <luisa.spinola9@gmail.com>
2021-03-09 20:06:47 -0300

    resolução do desafio 8 - empréstimo

---
+++ emprestimo.js
@@ -0,0 +1,14 @@
+function solucao(ganhos, serasaScore, mesesParaPagar) {
+    let juros = 0 ;
+      
+      if(serasaScore<300){
+          juros = 3;
+      } else if(serasaScore >= 300 && serasaScore< 700){
+          juros = 9;
+      }
+      else{
+          juros = 15;
+      }
+      let parcela=((ganhos*3) + (ganhos * (juros/100))) / mesesParaPagar;
+      console.log(parcela);
+  }
